f33e7b27acdd227603cc4b2da0db8f3876271f61
Frank Masuelli <fmasuelli1@outlook.com>
2024-06-19 12:57:10 -0700

    interactions manager

--- main.gd
+++ main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const HubPath = "res://scenes/levels/hub/hub.tscn"
+
 func _input(event):
 	if event is InputEventKey:
-		get_tree().change_scene_to_file("res://level_scenes/hub/hub.tscn")
+		get_tree().change_scene_to_file(HubPath)
