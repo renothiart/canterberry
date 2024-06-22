726954486d49266637d2752ee5aeac4901b5e853
Reno Thiart <rhthiart@gmail.com>
2024-06-21 21:12:49 -0400

    add animated intro

--- main.gd
+++ main.gd
@@ -2,6 +2,17 @@ extends Node2D
 
 const HubPath = "res://scenes/levels/hub/hub.tscn"
 
+@onready var _animation_player = $AnimationPlayer
+
+func _ready():
+	_animation_player.queue("splash_screen")
+	_animation_player.queue("title_screen")
+
 func _input(event):
-	if event is InputEventKey:
-		get_tree().change_scene_to_file(HubPath)
+	if event is InputEventKey && event.is_pressed():
+		if _animation_player.current_animation == "splash_screen":
+			_animation_player.seek(_animation_player.get_animation("splash_screen").length)
+		elif _animation_player.current_animation == "title_screen":
+			_animation_player.seek(_animation_player.get_animation("title_screen").length)
+		else:
+			get_tree().change_scene_to_file(HubPath)
